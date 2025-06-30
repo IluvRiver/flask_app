7c18d992d96a468316f21d53ceb87854f43e5809
IluvRiver <leecm2468@gmail.com>
2025-06-30 18:55:41 +0900

    gcp code update

--- app_gcp.py
+++ app_gcp.py
@@ -15,23 +15,11 @@ def get_secret(project_id, secret_id, version_id="latest"):
     client = secretmanager.SecretManagerServiceClient()
     name = client.secret_version_path(project_id, secret_id, version_id)
     response = client.access_secret_version(request={"name": name})
-    return response.payload.data.decode("UTF-8")
-
-# 각 설정값을 개별적으로 가져오기
-def get_config():
-    PROJECT_ID = "hifrodo-05"
-    return {
-        'flask_secret': get_secret(PROJECT_ID, "flask_secret"),
-        'mysql_db': get_secret(PROJECT_ID, "mysql_db"),
-        'mysql_host': get_secret(PROJECT_ID, "mysql_host"),
-        'mysql_password': get_secret(PROJECT_ID, "mysql_password"),
-        'mysql_user': get_secret(PROJECT_ID, "mysql_user"),
-        'redis_host': get_secret(PROJECT_ID, "redis_host")
-    }
-
+    return json.loads(response.payload.data.decode("UTF-8"))
 
 # 🔐 시크릿 로드
-secret = get_config()
+PROJECT_ID = "hifrodo-05"
+secret = get_secret(PROJECT_ID, "project-secrets")
 
 # Flask 앱 설정
 app = Flask(__name__)
